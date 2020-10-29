8e9d09345dae9ca330818fa93e5f52c835ce14b3
Igor Gorlov <gorlov35@gmail.com>
2020-10-29 20:22:44 +0300

    init

---
+++ markup/assets/js/main.js
@@ -0,0 +1,15 @@
+import svg4everybody from 'svg4everybody';
+
+// import $ from 'jquery';
+// import slick from 'slick-carousel';
+
+(function ($) {
+
+svg4everybody();
+
+$('.open-popup').magnificPopup({
+  type:'inline',
+  // midClick: true
+});
+
+})(jQuery);
